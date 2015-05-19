bb76668d3e465160dc7d13c03d4922ad53bf4a22
JayFeng <1025657562@qq.com>
2015-05-19 14:11:57 +0800

    新建了一个测试类

---
+++ newProject/entity/TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  newProject
+//
+//  Created by apple on 15-5-19.
+//  Copyright (c) 2015年 Harrison. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ newProject/entity/TestClass.m
@@ -0,0 +1,13 @@
+//
+//  TestClass.m
+//  newProject
+//
+//  Created by apple on 15-5-19.
+//  Copyright (c) 2015年 Harrison. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@implementation TestClass
+
+@end
